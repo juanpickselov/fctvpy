688e8598ff23d594f6689a7b06017b27d6dde7f5
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-10-19 19:57:05 -0400

    item 06 stuff

--- 01/item_06.py
+++ 01/item_06.py
@@ -3,7 +3,11 @@ color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 odds = color_list[::2]
 evens = color_list[1::2]
 backwards = color_list[::-1]
+backwards_by_two = color_list[::-2]
+backwards_even = color_list[-2::-2]
 
 print(odds)
 print(evens)
 print(backwards)
+print(backwards_by_two)
+print(backwards_even)
