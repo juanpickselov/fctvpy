e32b2903ec514edb95e2b2a0d6470e5c046f5e55
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-10-19 19:49:35 -0400

    item 06 stuff

---
+++ 01/item_06.py
@@ -0,0 +1,9 @@
+color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+odds = color_list[::2]
+evens = color_list[1::2]
+backwards = color_list[::-1]
+
+print(odds)
+print(evens)
+print(backwards)
