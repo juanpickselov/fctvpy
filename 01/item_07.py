73e6bfb974dd1dd3e78a5c79fdb161db28dc52ba
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-10-19 20:09:15 -0400

    item 07 stuff

--- 01/item_07.py
+++ 01/item_07.py
@@ -1,6 +1,10 @@
 list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squares = [x**2 for x in list_of_numbers]
-print(squares)
-
 squares_too = map(lambda x: x ** 2, list_of_numbers)
+even_squares = [x**2 for x in list_of_numbers if x % 2 == 0]
+odd_squares = [x**2 for x in list_of_numbers if x % 2 != 0]
+print(squares)
 print(list(squares_too))
+print(even_squares)
+print(odd_squares)
+
